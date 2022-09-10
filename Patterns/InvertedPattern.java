deebca18077a61aab375b1999c672b8991448696
Syed Suleman <sydsulmn7@gmail.com>
2022-09-10 22:16:45 +0530

    Inverted rotate Pyramid

--- Patterns/InvertedPattern.java
+++ Patterns/InvertedPattern.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class InvertedPattern {
     public static void main(String[] args) {
-        int num=5;
-        for(int i=num;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please Enter a Number");
+        int n = sc.nextInt();
+        invertedRotatePyramid(n);
+        sc.close();
+        
+    }
+    public static void invertedRotatePyramid(int num){
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=num-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
                 System.out.print("*");
             }
             System.out.println();
+            
         }
     }
 }

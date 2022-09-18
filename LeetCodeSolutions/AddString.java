2695e1deb6bc881fc381c110570244342f413eac
Syed Suleman <sydsulmn7@gmail.com>
2022-09-18 08:35:00 +0530

    add string

---
+++ LeetCodeSolutions/AddString.java
@@ -0,0 +1,24 @@
+package LeetCodeSolutions;
+public class AddString {
+    public static void main(String[] args) {
+        String result = addStrings("79","786");
+        System.out.println(result);
+    }
+    public static String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+     StringBuilder resultBuilder = new StringBuilder();
+     while ( i >= 0 || j >= 0 ) {
+         int sum = 0;
+         if ( i >= 0 ) sum += num1.charAt(i--) - '0';
+         if ( j >= 0 ) sum += num2.charAt(j--) - '0';
+         sum += carry;
+         carry = sum / 10;
+         sum = sum % 10;
+         resultBuilder.append(sum);
+     }
+     if ( carry > 0 ) resultBuilder.append(carry);
+     return resultBuilder.reverse().toString();   
+     }
+}

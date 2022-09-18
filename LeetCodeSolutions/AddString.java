18267f7a983bc8b3d5b58672c9819dcaa9d48da6
Syed Suleman <sydsulmn7@gmail.com>
2022-09-18 08:37:30 +0530

    415. Add Strings

--- LeetCodeSolutions/AddString.java
+++ LeetCodeSolutions/AddString.java
@@ -4,14 +4,15 @@ public class AddString {
         String result = addStrings("79","786");
         System.out.println(result);
     }
+    //following school addition method with carry
     public static String addStrings(String num1, String num2) {
         int i = num1.length() - 1;
         int j = num2.length() - 1;
         int carry = 0;
-     StringBuilder resultBuilder = new StringBuilder();
+     StringBuilder resultBuilder = new StringBuilder(); //to append and store the result/answer
      while ( i >= 0 || j >= 0 ) {
          int sum = 0;
-         if ( i >= 0 ) sum += num1.charAt(i--) - '0';
+         if ( i >= 0 ) sum += num1.charAt(i--) - '0'; //converting string to integer
          if ( j >= 0 ) sum += num2.charAt(j--) - '0';
          sum += carry;
          carry = sum / 10;
